c51586154960e52208d1ba7deae0b4c47dda8635
Max Melin <mmelin@g.ucla.edu>
2023-07-11 16:36:27 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Install script for some random utils.
+# Max Melin
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = 'encoding_tools',
+    version = '0.1.0',
+    author = 'Max Melin',
+    author_email = 'mmelin@g.ucla.edu',
+    description = 'None',
+    packages = find_packages()
+)
